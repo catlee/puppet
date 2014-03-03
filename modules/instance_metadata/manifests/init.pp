ae1804f1f994aaefee28e514a8db3298eac6e3e6
Chris AtLee <catlee@mozilla.com>
2014-03-03 17:12:51 -0500

    use fqdn regexes

--- modules/instance_metadata/manifests/init.pp
+++ modules/instance_metadata/manifests/init.pp
@@ -3,21 +3,35 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class instance_metadata {
-    include packages::mozilla::py27_mercurial
-    $python = $::packages::mozilla::python27::python
+    case $::fqdn {
+        /.*\.releng\.(use1|usw2)\.mozilla\.com$/: {
+            # AWS machines should fetch instance metadata and dump it in /etc/instance_metadata.json
+            include packages::mozilla::py27_mercurial
+            $python = $::packages::mozilla::python27::python
 
-    file {
-        "/usr/local/bin/instance_metadata.py":
-            owner  => "root",
-            mode   => 0755,
-            source => "puppet:///modules/instance_metadata/instance_metadata.py";
+            file {
+                "/usr/local/bin/instance_metadata.py":
+                    owner  => "root",
+                    mode   => 0755,
+                    source => "puppet:///modules/instance_metadata/instance_metadata.py";
 
-    }
+            }
 
-    cron {
-        require => File["/usr/local/bin/instance_metadata.py"],
-        user    => root,
-        command => "$python /usr/local/bin/instance_metadata.py -o /etc/instance_metadata.json",
-        special => "reboot";
+            cron {
+                require => File["/usr/local/bin/instance_metadata.py"],
+                user    => root,
+                command => "$python /usr/local/bin/instance_metadata.py -o /etc/instance_metadata.json",
+                special => "reboot";
+            }
+        }
+        default: {
+            # Non-AWS machines should have empty metadata
+            file {
+                "/etc/instance_metadata.json":
+                    owner    => root,
+                    mode     => 0644,
+                    contents => "{}";
+            }
+        }
     }
 }
