a6a3090a1577dec41addc0cab33a0d271ae59e81
Chris AtLee <catlee@mozilla.com>
2015-07-02 10:28:54 -0400

    use proper group on OSX

--- modules/mercurial/manifests/system_hgrc.pp
+++ modules/mercurial/manifests/system_hgrc.pp
@@ -3,16 +3,22 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class mercurial::system_hgrc {
     include mercurial::settings
+
+    $group = $::operatingsystem ? {
+        Darwin => wheel,
+        default => root
+    }
+
     file {
         $mercurial::settings::hgrc_parentdirs:
             ensure => directory,
             owner => "root",
-            group => "root";
+            group => $group;
     }
 
     mercurial::hgrc {
         "$mercurial::settings::hgrc":
             owner => "root",
-            group => "root";
+            group => $group;
     }
 }
